940693ac575eec389104e46db595d596b944207a
Miles Wells <80766315+MilesWellsSVT@users.noreply.github.com>
2024-11-07 12:59:06 -0500

    hotfix work (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Environment: {process.env.NEXT_PUBLIC_ENV}</h1>
         <h2>Feature A</h2>
+        <h2>Hotfix</h2>
       </main>
     </div>
   );
