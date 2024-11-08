5e4cb6283d868494c5de2c6fc024d93c36c4896d
Miles Wells <miles@svtrobotics.com>
2024-11-08 10:47:36 -0500

    upgrade cache action version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Environment: {process.env.NEXT_PUBLIC_ENV}</h1>
         <Button>Button</Button>
-        <h2>Feature A</h2>
       </main>
     </div>
   );
