65d5fee64856024bf103c17d0817a768010f1522
Miles Wells <miles@svtrobotics.com>
2024-11-08 14:06:49 -0500

    feature a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Environment: {process.env.NEXT_PUBLIC_ENV}</h1>
         <Button>Button</Button>
+        <h2>Feature A</h2>
       </main>
     </div>
   );
